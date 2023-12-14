e26d403a884a9ff57f542cc08f7d3c82e57d7e3a
Brletic <drew.brletic@afs.com>
2023-12-14 13:42:17 -0500

    Added in branch to cronjob table

--- src/main/java/ffm/cms/OpenshiftResource.java
+++ src/main/java/ffm/cms/OpenshiftResource.java
@@ -15,6 +15,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import net.redhogs.cronparser.CronExpressionDescriptor;
 import io.fabric8.tekton.client.*;
+import io.fabric8.tekton.triggers.v1alpha1.Param;
 import io.fabric8.tekton.triggers.v1alpha1.TriggerBinding;
 
 import org.jboss.resteasy.reactive.RestPath;
@@ -46,24 +47,29 @@ public class OpenshiftResource {
 
         // Helpful openShiftClient / kubernetes cheatsheet
         //https://github.com/fabric8io/kubernetes-client/blob/main/doc/CHEATSHEET.md#cronjob
-
-        //https://www.npmjs.com/package/cronstrue
-        // To try to get the TiggerBinding https://github.com/fabric8io/kubernetes-client/blob/main/kubernetes-examples/src/main/java/io/fabric8/kubernetes/examples/GenericKubernetesResourceExample.java
-        //https://stackoverflow.com/questions/40858456/how-to-display-a-javascript-var-in-html-body
-
+        
+        //Having to use TektonClient for anything related to pipelines
         TektonClient tknClient = new KubernetesClientBuilder().build().adapt(TektonClient.class);
+        Map<String, String> bindingParamsToBranch = new HashMap<String, String>();
         List<CronJobData> cronJobs = new ArrayList<>();
         List<CronJob> cronJobList = openshiftClient.batch().v1().cronjobs().inNamespace(namespace).list().getItems();
        
-        System.out.print("Listing Cronjob in Namespace: " + namespace);
+        List<TriggerBinding> tbList =  tknClient.v1alpha1().triggerBindings().inNamespace(namespace).list().getItems();   
+        for(TriggerBinding tb : tbList){
+           List<Param> params =  tb.getSpec().getParams();
+           for(Param param: params){
+                if(param.getName().equals("releaseBranch")){
+                    bindingParamsToBranch.put(tb.getMetadata().getName(), param.getValue());
+                }
+           }
+        }
         for(CronJob job : cronJobList){
             CronJobData currentJob = new CronJobData();
             currentJob.name = job.getMetadata().getName();
             currentJob.schedule = job.getSpec().getSchedule();
             currentJob.humanReadableMsg = CronExpressionDescriptor.getDescription(currentJob.schedule);
-            TriggerBinding currentBinding =  tknClient.v1alpha1().triggerBindings().inNamespace(namespace).withName(currentJob.name + "-tb").get();
-            Map<String, Object> bindingParams  = currentBinding.getAdditionalProperties();
-            currentJob.branch = (String) bindingParams.get("releaseBranch");
+            String bindingName = currentJob.name + "-binding";
+            currentJob.branch = bindingParamsToBranch.get(bindingName);
             cronJobs.add(currentJob);
         }
         return Templates.cronJobData(cronJobs);
