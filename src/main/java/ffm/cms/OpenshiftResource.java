9a816233ad99310c38731b8f6bf873b0b21a3d70
Andrew Brletich <dbrletic@redhat.com>
2024-03-02 16:10:44 -0500

    Added in getCronJob

--- src/main/java/ffm/cms/OpenshiftResource.java
+++ src/main/java/ffm/cms/OpenshiftResource.java
@@ -153,6 +153,25 @@ public class OpenshiftResource {
     
     }
 
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/{namespace}/{cronJobName}")
+    public String getCronJobSchedule(@RestPath String namespace, @RestPath String cronJobName) throws IOException, ParseException{
+        
+        System.out.println("Namepace: " + namespace  +" Cronjob: " + cronJobName);
+
+        //Checking to see if the given CronJob is in the system
+        if (openshiftClient.batch().v1beta1().cronjobs().inNamespace(namespace).withName(cronJobName) == null)
+            return cronJobName + " not found";
+        
+
+        return openshiftClient.batch().v1beta1().cronjobs().inNamespace(namespace).withName(cronJobName)
+                              .get().getSpec().getSchedule();
+       
+    
+    }
+
+
 
 
     /**
