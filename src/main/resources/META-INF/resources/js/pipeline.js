1b64f0c9742af321d27e6a00cc630cd07b77f1a9
Andrew Brletich <dbrletic@redhat.com>
2024-10-21 10:06:19 -0400

    Updated pipeline.js

--- src/main/resources/META-INF/resources/js/pipeline.js
+++ src/main/resources/META-INF/resources/js/pipeline.js
@@ -41,7 +41,7 @@ const serialize_form = form => JSON.stringify(
       error: function(xhr, status, error) {
         $("#successMessage").hide();     
         console.log("Error: " + error);
-        $("#cronForum")[0].reset();
+        $("#piplelineRunForm")[0].reset();
         $("<p>Error: " + error +"</p>").appendTo('#errorMessage');
       }
     });
@@ -120,50 +120,4 @@ const serialize_form = form => JSON.stringify(
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
-  
-  /* Allows you to search the tables */
-  function searchTables() {
-    const searchInput = document.getElementById('searchInput');
-    const searchValue = searchInput.value.trim();
-    const rowIndex = 0; // Search in the first column
-    const tables = [
-      document.getElementById('test1'),
-      document.getElementById('test2'),
-      document.getElementById('test3'),
-      document.getElementById('test4')
-    ];
-  
-    tables.forEach(table => {
-      const rows = table.rows;
-      const searchRegex = new RegExp(searchValue, 'i'); // Create a case-insensitive regular expression
-  
-      // Iterate over each row in the table
-      for (let i = 1; i < rows.length; i++) { // Skip the header row
-        const row = rows[i];
-        const cell = row.cells[rowIndex];
-        const cellText = cell.textContent.trim();
-  
-        // Check if the cell in the specified row contains the search value
-        if (searchRegex.test(cellText)) {
-          row.style.display = ''; // Show the row
-        } else {
-          row.style.display = 'none'; // Hide the row
-        }
-      }
-    });
-    }
-  
-    /* Allows for extending of failures */
-    addEventListener('DOMContentLoaded', function() { 
-      var test1 = document.getElementById('test1'); 
-      
-      test1.addEventListener('click', function(e) {
-         if (e.target.classList.contains('reveal-cell')){ 
-            e.target.classList.toggle('expanded'); 
-          } 
-      });
-      
-      
-     }); 
-     
   
